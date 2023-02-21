669bb33f6d9344073ca821a5a4144f64e414f454
josem.ferreira@iesnervion.es <josem.ferreira@iesnervion.es>
2023-02-21 10:23:39 +0100

    mondongo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
     private static Conexion conexion = new Conexion();
     public static void main(String[] args) {
-        conexion.conectar();
+        conexion.conectar(); //hola
 
     }
 
